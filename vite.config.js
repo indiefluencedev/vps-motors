b82d62cd51e1eda4069b3abbedfd9185aaf4dfb6
gaurav23617 <gaurav23617@gmail.com>
2025-08-25 15:08:33 +0530

    biome added

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
